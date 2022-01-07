9543c0d177f6d7d5f6b04a383d5a5846544dc8c7
BlackMarine <wkdeo0000@gmail.com>
2022-01-07 17:11:36 +0900

    #2.5 기본 틀 완료

--- App.js
+++ App.js
@@ -5,8 +5,36 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello i made a RN app!!!!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,12 +42,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1,
+    backgroundColor: "blue",
     justifyContent: "center",
+    alignItems: "center",
   },
-  text: {
-    fontSize: 30,
-    color: "red",
+  cityName: {
+    fontSize: 38,
+    fontWeight: "700",
   },
+  weather: {
+    flex: 3,
+    // backgroundColor: "teal",
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+    // backgroundColor: "teal",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 98,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 50,
+  }
 });
