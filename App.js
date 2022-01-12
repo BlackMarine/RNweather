750da78f7935784c272c60922c9dc7800923fe85
BlackMarine <wkdeo0000@gmail.com>
2022-01-12 12:54:27 +0900

    날씨정보 연결 메인 온도, 설명, 작은설명

--- App.js
+++ App.js
@@ -1,44 +1,56 @@
-
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { getPermissionsAsync } from "expo-location";
-import * as Location from 'expo-location';
+import * as Location from "expo-location";
 import { StatusBar } from "expo-status-bar";
 import reactDom from "react-dom";
-import { StyleSheet, Text, View, ScrollView } from "react-native"; //스크롤뷰
-import { Dimensions } from 'react-native'; //치수
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native"; //스크롤뷰
+import { Dimensions } from "react-native"; //치수
 
-const SECREEN_WIDTH = Dimensions.get('window').width;
+const SECREEN_WIDTH = Dimensions.get("window").width;
 console.log(SECREEN_WIDTH);
 const API_KEY = "784ab24ff2ed5d94d4288abed9e25d13";
 
 export default function App() {
-  const [city, setCity] = useState("Loading...")
+  const [city, setCity] = useState("Loading...");
   // const [location, setLocation] = useState();
   const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
 
-  const getWeather = async() => {
+  const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
 
     //step 1 사용자 권한 받기
-    if(!granted){
+    if (!granted) {
       setOk(false);
     }
 
-    const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy:5})
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
 
-    const location = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps:false})
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
 
     setCity(location[0].city); // 내 도시
     //step 2 해당 위치를 API에 전송하고 날씨를 가져와야함
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}`)
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric `
+    );
     const json = await response.json();
     setDays(json.daily);
-  }
+  };
 
   useEffect(() => {
     getWeather();
-  }, [])
+  }, []);
   //컴포넌트가 마운트 되면 useEffect를 사용해서 getPermissions function을호출
   return (
     <View style={styles.container}>
@@ -51,31 +63,26 @@ export default function App() {
         horizontal
         showsHorizontalScrollIndicator={false} //하단막대
         //indicatorStyle="white" //아이폰만 가능한 인디케이터 색상변경 props
-        contentContainerStyle={styles.weather}>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+        contentContainerStyle={styles.weather}
+      >
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator
+              color="white"
+              style={{ marginTop: 10 }}
+              size="large"
+            />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
@@ -113,5 +120,8 @@ const styles = StyleSheet.create({
   description: {
     marginTop: -30,
     fontSize: 50,
+  },
+  tinyText: {
+    fontSize: 30,
   }
 });
